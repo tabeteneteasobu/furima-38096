3451af31fca8851904513bf2bcd309d9f0383668
tabeteneteasobu <keeei.yamada@gmail.com>
2022-06-21 15:58:20 +0900

    新規登録のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,15 @@ class User < ApplicationRecord
          validates :email, presence: true
          validates :encrypted_password, presence: true
          validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :birthday, presence: true
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
 
 end
+
