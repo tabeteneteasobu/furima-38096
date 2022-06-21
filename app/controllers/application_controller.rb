d560c3afbcce655b49628803ee36606217023f0f
tabeteneteasobu <keeei.yamada@gmail.com>
2022-06-21 16:08:09 +0900

    Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,12 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
-  end
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,
+                                        keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+    end
   end
-end
 end
