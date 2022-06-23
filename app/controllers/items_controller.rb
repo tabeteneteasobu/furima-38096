07f164b8752cb177b1f235342e874d259a59a3e2
tabeteneteasobu <keeei.yamada@gmail.com>
2022-06-23 16:29:16 +0900

    Active Storageの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,29 @@ class ItemsController < ApplicationController
   def index
   end
 
-  def show
+  def new
+    @item = Item.new
+    @category = Category.all
+    @condition = Condition.all
+    @postage_payer = PostagePayer.all
+    @prefecture = Prefecture.all
+    @shipment_date = ShipmentDate.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :shipment_date_id, :price, :image).merge(user_id: current_user.id)
   end
 end
+
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,29 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage_payer
+  belongs_to :prefecture
+  belongs_to :shipment_date
+
+
   belongs_to :user
 
   has_one_attached :image
 
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :postage_payer_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipment_date_id, presence: true
+  validates :price, presence: true,           format: { with: /\A[0-9]+\z/ }
+  validates :price,
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  validates :user, presence: true
+  validates :image, presence: true
 
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
 end
