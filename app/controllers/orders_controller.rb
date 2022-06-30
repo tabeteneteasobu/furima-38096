e521d8a7f8dbc777ecb0fce1bebe578a115a2c23
tabeteneteasobu <keeei.yamada@gmail.com>
2022-06-30 19:36:08 +0900

    set_itemのメソッド化

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,13 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :move_to_index
+  before_action :set_item, only: [:index, :create, :move_to_index]
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -37,7 +36,10 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:item_id])
     redirect_to root_path if (current_user.id == @item.user.id) || @item.order.present?
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
