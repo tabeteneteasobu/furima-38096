94cde5ad69c96d655b04a6dbf575ca72e6bf19f2
tabeteneteasobu <keeei.yamada@gmail.com>
2022-07-03 13:36:21 +0900

    SnsCredentialモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
+  #バリデーション
   validates :nickname, presence: true
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]/ }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]/ }
@@ -17,4 +16,5 @@ class User < ApplicationRecord
   # アソシエーション
   has_many :items
   has_many :orders
+  has_many :sns_credentials
 end
